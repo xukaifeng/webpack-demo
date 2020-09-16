8cf38bf2c66dc669d9c8236553d2f0eb436fa2c4
kaifeng <953555581@qq.com>
2020-09-16 23:45:45 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import _ from 'lodash';
-import styles from './index.less';
+import Home from '@src/pages/home';
+// import styles from './index.less';
 
 class App extends React.Component {
   render() {
-    const test = { a: 1 };
-    const temp = _.cloneDeep(test);
-    temp.a = 3;
-    return <div className={styles.testBox}>hello world</div>;
+    return <Home />;
   }
 }
 
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button } from 'antd';
+import styles from './index.less';
+
+interface IProps {}
+
+interface IState {
+  count: number;
+}
+
+class Home extends React.Component<IProps, IState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  public render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <Button onClick={this.handleClick}>Click me</Button>
+        <div>{count}</div>
+      </div>
+    );
+  }
+
+  public handleClick() {
+    const { count } = this.state;
+    this.setState({ count: count + 1 });
+  }
+}
+
+export default Home;
