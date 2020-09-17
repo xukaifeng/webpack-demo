605f3900d9bce1fbeeb3d2a14a5798d2939b53e3
kaifeng <953555581@qq.com>
2020-09-17 22:32:03 +0800

    test script

---
+++ scripts/push.js
@@ -0,0 +1,11 @@
+const shell = require('shelljs');
+const git = require('simple-git');
+const path = require('path');
+const dir = path.join(__dirname, '../');
+
+git(dir).branch((err, res) => {
+  console.log('current branch:', res.current);
+  const curBranch = res.current;
+  const commitMsg = process.argv[2] || 'update';
+  shell.exec(`git add . && git commit -m "${commitMsg}" && git push origin ${curBranch}`);
+});
