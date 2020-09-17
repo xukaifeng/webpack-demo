1cd1e449c3e34d554b40248e8ac5bcf5d09930f2
kaifeng <953555581@qq.com>
2020-09-17 22:36:20 +0800

    update

--- scripts/push.js
+++ scripts/push.js
@@ -3,6 +3,7 @@ const git = require('simple-git');
 const path = require('path');
 const dir = path.join(__dirname, '../');
 
+// npm run push 'commit message'
 git(dir).branch((err, res) => {
   console.log('current branch:', res.current);
   const curBranch = res.current;
